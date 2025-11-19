7ac21a989a88844417ccc31c1cb60965a1d91392
Muhammad Farhan Adisaputro <@z5435593.ad.unsw.edu.au>
2025-11-20 00:02:16 +1100

    Update App.jsx to use   HostedListings component

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -3,10 +3,10 @@ import { AuthProvider } from './contexts/AuthContext';
 import NavBar from './components/navigation/NavBar';
 import Login from './pages/Login';
 import Register from './pages/Register';
+import HostedListings from './pages/HostedListings';
 
 // Temporary placeholder components - we'll create these in future commits
 const AllListings = () => <div>All Listings - Coming Soon</div>;
-const HostedListings = () => <div>Hosted Listings - Coming Soon</div>;
 const EditListing = () => <div>Edit Listing - Coming Soon</div>;
 const ViewListing = () => <div>View Listing - Coming Soon</div>;
 const BookingRequests = () => <div>Booking Requests - Coming Soon</div>;

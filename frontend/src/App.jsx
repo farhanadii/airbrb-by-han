6d74910deecad45a66f2230c044a4f24e84f5414
Muhammad Farhan Adisaputro <@z5435593.ad.unsw.edu.au>
2025-11-23 12:56:14 +1100

    Apply theme provider to App

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,4 +1,5 @@
 import { BrowserRouter, Routes, Route, Navigate } from 'react-router-dom';
+import { ThemeProvider, CssBaseline, Box } from '@mui/material';
 import { AuthProvider } from './contexts/AuthContext';
 import { NotificationsProvider } from './contexts/NotificationsContext';
 import NavBar from './components/navigation/NavBar';
@@ -10,28 +11,34 @@ import EditListing from './pages/EditListing';
 import AllListings from './pages/AllListings';
 import ViewListing from './pages/ViewListing';
 import BookingRequests from './pages/BookingRequests';
+import theme from './theme';
 
 function App() {
   return (
-    <AuthProvider>
-      <NotificationsProvider>
-        <BrowserRouter>
-          <NavBar />
-          <Routes>
-            <Route path="/" element={<AllListings />} />
-            <Route path="/login" element={<Login />} />
-            <Route path="/register" element={<Register />} />
-            <Route path="/hosted" element={<HostedListings />} />
-            <Route path="/hosted/new" element={<CreateListing />} />
-            <Route path="/listings/:id" element={<ViewListing />} />
-            <Route path="/listings/:id/edit" element={<EditListing />} />
-            <Route path="/listings/:id/bookings" element={<BookingRequests />}
-            />
-            <Route path="*" element={<Navigate to="/" />} />
-          </Routes>
-        </BrowserRouter>
-      </NotificationsProvider>
-    </AuthProvider>
+    <ThemeProvider theme={theme}>
+      <CssBaseline />
+      <AuthProvider>
+        <NotificationsProvider>
+          <BrowserRouter>
+            <Box sx={{ minHeight: '100vh', bgcolor: 'background.default' }}>
+              <NavBar />
+              <Routes>
+                <Route path="/" element={<AllListings />} />
+                <Route path="/login" element={<Login />} />
+                <Route path="/register" element={<Register />} />
+                <Route path="/hosted" element={<HostedListings />} />
+                <Route path="/hosted/new" element={<CreateListing />} />
+                <Route path="/listings/:id" element={<ViewListing />} />
+                <Route path="/listings/:id/edit" element={<EditListing />} />
+                <Route path="/listings/:id/bookings" element={<BookingRequests />}
+                />
+                <Route path="*" element={<Navigate to="/" />} />
+              </Routes>
+            </Box>
+          </BrowserRouter>
+        </NotificationsProvider>
+      </AuthProvider>
+    </ThemeProvider>
   );
 }
 
